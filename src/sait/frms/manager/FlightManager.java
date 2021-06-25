72e64061cc5c735a14af23a8a89d0443f26fdcdc
sbn073 <Scott.B.Normore@gmail.com>
2021-06-25 12:48:39 -0600

    Added code to classes

--- src/sait/frms/manager/FlightManager.java
+++ src/sait/frms/manager/FlightManager.java
@@ -1,5 +1,47 @@
 package sait.frms.manager;
 
-public class FlightManager {
+import java.util.ArrayList;
+import sait.frms.problemdomain.*;
 
+public class FlightManager {
+	public final static String WEEKDAY_ANY="ANY";
+	public final static String WEEKDAY_SUNDAY="SUNDAY";
+	public final static String WEEKDAY_MONDAY="MONDAY";
+	public final static String WEEKDAY_TUESDAY="TUESDAY";
+	public final static String WEEKDAY_WEDNESDAY="WEDNESDAY";
+	public final static String WEEKDAY_THURSDAY="THURSDAY";
+	public final static String WEEKDAY_FRIDAY="FRIDAY";
+	public final static String WEEKDAY_SATURDAY="SATURDAY";
+
+	private ArrayList<Flight> flights;
+	private ArrayList<String> airports;
+	
+	public FlightManager() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ArrayList<Flight> getFlights() {
+		return flights;
+	}
+
+	public ArrayList<String> getAirports() {
+		return airports;
+	}
+	
+	public String findAirportByCode(String code) {
+		return "";
+	}
+	public Flight findFlightByCode(String code) {
+
+	}
+	public ArrayList<Flight> findFlights(String from, String to, String weekday){ 
+	}
+	private void populateFlights() {
+		
+	}
+	private void populateAirports() {
+		
+	}
+	
 }
--- src/sait/frms/manager/ReservationManager.java
+++ src/sait/frms/manager/ReservationManager.java
@@ -1,5 +1,34 @@
 package sait.frms.manager;
 
-public class ReservationManager {
+import java.util.ArrayList;
+import sait.frms.problemdomain.*;
 
+public class ReservationManager {
+	private ArrayList<Reservation> reservations;
+
+	public ReservationManager() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Reservation makeReservation(Flight flight, String name, String citizenship) {
+		
+	}
+	public ArrayList<Reservation> findReservations(String code, String airline, String name) {
+		
+	}
+	public Reservation findReservationByCode(String code){
+		
+	}
+	public void persist() {
+		
+	}
+	private int getAvaliableSeats(Flight flight){
+		
+	}
+	private String generateReservationCode(Flight flight){
+		
+	}
+	private void populateFromBinary(){
+		
+	}
 }
